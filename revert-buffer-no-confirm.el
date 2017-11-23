eb842683388d2c545633d92b3f5f04d403c5afec
Tim Loderhose <tim@loderhose.com>
2017-11-23 08:34:50 -0600

    Add revert-buffer-no-confirm to refresh buffer

---
+++ revert-buffer-no-confirm.el
@@ -0,0 +1,6 @@
+;; source: https://www.emacswiki.org/emacs/misc-cmds.el
+(defun revert-buffer-no-confirm ()
+    "Revert buffer without confirmation."
+    (interactive)
+    (revert-buffer :ignore-auto :noconfirm))
+(provide 'revert-buffer-no-confirm)
